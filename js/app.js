7edb414186b4cde0a4d235c3be8aecae083625f1
Marcus Davi Argolo Ribeiro <marcusdavi@gmail.com>
2019-03-07 22:28:39 -0300

    Add New Cats

--- js/app.js
+++ js/app.js
@@ -1,16 +1,86 @@
+var initialCats = [
+        {
+            clickCount : 0,
+            name : 'Tabby',
+            imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/bigtallguy/434164568',
+            nicknames: ['Tabtab', 'T-Bone', 'Mr. T', 'Tabitha Tab Tabby Catty Cat']
+        },
+        {
+            clickCount : 0,
+            name : 'Tiger',
+            imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/xshamx/4154543904',
+            nicknames: ['Ti', 'TiTi']
+        },
+        {
+            clickCount : 0,
+            name : 'Scaredy',
+            imgSrc : 'img/22252709_010df3379e_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/kpjas/22252709',
+            nicknames: ['Scar', 'Isc']
+        },
+        {
+            clickCount : 0,
+            name : 'Shadow',
+            imgSrc : 'img/1413379559_412a540d29_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/malfet/1413379559',
+            nicknames: ['Sha', 'Shasha', 'Shaw']
+        },
+        {
+            clickCount : 0,
+            name : 'Sleepy',
+            imgSrc : 'img/9648464288_2516b35537_z.jpg',
+            imgAttribution : 'https://www.flickr.com/photos/onesharp/9648464288',
+            nicknames: ['Is', 'Leep']
+        }
+    ]
+
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable (data.imgAttribution);
+    this.nicknames = ko.observableArray(data.nicknames);
+    
+    this.title = ko.computed(function(){
+        var title;
+        var clicks = this.clickCount();
+        if (clicks < 10){
+            title = 'Newborn';
+        } else if (clicks < 50){
+            title = 'Infant';
+        } else if (clicks < 100){
+            title = 'Child'
+        } else if (clicks < 200){
+           title = 'Teen' 
+        } else if (clicks < 500){
+           title = 'Adult';
+        } else {
+           title = 'Ninja'; 
+        }
+        return title;
+    }, this);
+}
+
 var ViewModel = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('https://www.flickr.com/photos/bigtallguy/434164568');
+    var self = this;
+    
+    this.catList = ko.observableArray([]);
+    
+    initialCats.forEach(function(catItem){
+        self.catList.push( new Cat (catItem));
+    });
+    
+    this.currentCat = ko.observable(this.catList()[0]);
     
     this.incrementCounter = function(){
-        this.clickCount(this.clickCount() + 1);
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     }
     
-    this.level = ko.pureComputed(function(){
-        return this.clickCount() < 3 ? "New Born": this.clickCount() <= 10 ? "Infant" : "Teen";
-    }, this);
+    this.changeCurrentCat = function(clickedCat){
+        self.currentCat(clickedCat);
+    }
     
     
 }
